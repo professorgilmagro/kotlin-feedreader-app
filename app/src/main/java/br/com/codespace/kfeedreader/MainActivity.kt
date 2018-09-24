f3ef7921d8b9bb86bee0c8aba309fea9ea2d9fb6
Gilmar Soares <gilmar.santos@grupofolha.com.br>
2018-09-23 21:25:10 -0300

    Adiciona menu para atualização do feed

--- app/src/main/java/br/com/codespace/kfeedreader/MainActivity.kt
+++ app/src/main/java/br/com/codespace/kfeedreader/MainActivity.kt
@@ -5,24 +5,45 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
 import com.pkmmte.pkrss.Article
 import com.pkmmte.pkrss.Callback
 import com.pkmmte.pkrss.PkRSS
 
 class MainActivity : AppCompatActivity(), Callback {
-    lateinit var listView: RecyclerView
-    lateinit var adapter: RecyclerView.Adapter<ItemAdapter.ItemViewHolder>
-    var listItems = arrayListOf<Item>()
+    private lateinit var listView: RecyclerView
+    private var listItems = arrayListOf<Item>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
+    override fun onResume() {
         listView = findViewById(R.id.rvContent)
         listView.layoutManager = LinearLayoutManager(this)
         listView.adapter = ItemAdapter(listItems, this)
 
         PkRSS.with(this).load("https://rss.tecmundo.com.br/feed").callback(this).async()
+        super.onResume()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.refresh -> {
+                onResume()
+            }
+
+            R.id.exit -> finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onLoadFailed() {
@@ -35,6 +56,7 @@ class MainActivity : AppCompatActivity(), Callback {
     override fun onLoaded(newArticles: MutableList<Article>?) {
         listItems.clear()
         newArticles?.mapTo(listItems) {
+            var article = it
             Item(it.title, it.author, it.date, it.source, it.enclosure.url)
         }
 
